bdb1c3c57c480c32a64e7351db441ea15bb7596c
veryCrunchy <me@verycrunchy.dev>
2025-06-20 01:18:47 +0200

    Revert "Remove unused API components and add new service structure"
    This reverts commit ff931a3548305be5a3df7337ecd9e88111553170.

---
+++ apps/api/server/build.rs
@@ -0,0 +1,11 @@
+use std::env::vars;
+
+use dotenvy::dotenv;
+
+fn main() {
+    dotenv().ok();
+
+    for (k, v) in vars() {
+        println!("cargo:rustc-env={k}={v}");
+    }
+}
---
+++ apps/api/server/src/routes/mod.rs
@@ -0,0 +1,5 @@
+mod health;
+
+macros_utils::routes! {
+    load health,
+}

6d0a5375af3f2394652d26e32f2ebe7718ace7c5
Jayden Andrews <48751572+GreyStinger@users.noreply.github.com>
2025-06-22 03:33:00 +0200

    Add simple Args/TOML (#13)
    * added some toml config file support
    
    * added simple args and config parsing
    
    ---------
    
    Signed-off-by: veryCrunchy <me@verycrunchy.dev>
    Co-authored-by: veryCrunchy <me@verycrunchy.dev>

--- apps/service/src/main.rs
+++ apps/service/src/main.rs
@@ -1,27 +1,72 @@
+use std::{env, path};
+
+use clap::{Parser, crate_authors, crate_version};
+
+mod config;
 mod peer;
-use zmq;
-use std::env;
-fn get_env_var<T: std::str::FromStr>(name: &str, default: T) -> T {
-    match env::var(name) {
-        Ok(val) => val.parse().unwrap_or(default),
-        Err(_) => default,
+
+#[derive(Parser, Debug)]
+#[command(about = "Distributed monitoring system", long_about = None)]
+struct Args {
+    #[arg(short = 'V', long)]
+    /// Print version
+    version: bool,
+
+    #[arg(long)]
+    /// Path to specific config file
+    config: Option<path::PathBuf>,
+}
+
+struct ZmqConn {
+    pub ctx: zmq::Context,
+    pub sock: zmq::Socket,
+}
+
+impl ZmqConn {
+    pub fn new(cfg: &config::Config) -> Self {
+        let bind: String = env::var("BIND").unwrap_or_else(|_: env::VarError| {
+            // Debug print env err
+            String::clone(&cfg.zeromq.bind)
+        });
+
+        let port: u16 = match env::var("PORT") {
+            Ok(raw_val) => {
+                if let Ok(parsed_val) = raw_val.parse::<u16>() {
+                    parsed_val
+                } else {
+                    // Debug print bad env data (could not parse)
+                    cfg.zeromq.port
+                }
+            },
+            Err(_) => {
+                // Debug print env err
+                cfg.zeromq.port
+            },
+        };
+
+        let context = zmq::Context::new();
+        let socket = context.socket(zmq::REP).unwrap();
+        socket.bind(&format!("tcp://{bind}:{port}")).unwrap();
+
+        Self { ctx: context, sock: socket }
     }
+
+    pub fn poll(&self) {}
 }
 
 fn main() {
-    let bind: String = get_env_var("BIND", "*".to_string());
-    let port: u16 = get_env_var("PORT", 5555);
-    let context = zmq::Context::new();
-    let socket = context
-        .socket(zmq::REP)
-        .unwrap();
-    socket
-        .bind(&format!("tcp://{}:{}", bind, port))
-        .unwrap();
+    let cli = Args::parse();
+    if cli.version {
+        let authors = crate_authors!().split(':').collect::<Vec<&str>>().join("\", \"");
+        println!("PeerUP service {} - Authors: \"{}\"", crate_version!(), authors);
+        return;
+    }
+    let cfg = config::Config::from_config(cli.config.as_ref()).expect("Failed to fetch config");
 
-        // peer::announcer::(&socket);
-        // peer::announcer::(&socket);
+    let zmq_conn = ZmqConn::new(&cfg);
+
     loop {
-      
+        zmq_conn.poll();
+        std::thread::sleep(std::time::Duration::from_millis(100));
     }
 }

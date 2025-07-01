77e451157c35f78900515ed5164c141ce7c8892c
veryCrunchy <me@verycrunchy.dev>
2025-07-02 01:56:16 +0200

    iroh

--- apps/service/src/peer/identity.rs
+++ apps/service/src/peer/identity.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use iroh::{Endpoint, SecretKey};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let secret_key: SecretKey = SecretKey::generate(rand::rngs::OsRng);
+    println!("secret_key {secret_key}");
 }

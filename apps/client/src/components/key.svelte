9758746eac28092911ba0d757ee7718d502ebbd6
veryCrunchy <me@verycrunchy.dev>
2025-07-04 01:42:46 +0200

    chore: remove svelte components

--- apps/client/src/components/key.svelte
+++
@@ -1,1 +0,0 @@
-<kbd class="pointer-events-none flex h-6 min-w-[1.25rem] items-center justify-center rounded-md border border-muted/20 bg-muted/10 px-1 font-mono text-[11px] font-semibold text-subtle"><slot /></kbd>

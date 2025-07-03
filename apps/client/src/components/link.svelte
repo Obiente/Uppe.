6bb1f1e7d52e42b771db1b37dc019003e5efe334
veryCrunchy <me@verycrunchy.dev>
2025-07-03 03:21:33 +0200

    chore: add template components

---
+++ apps/client/src/components/link.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let href: Optional<string> = undefined;
+	export let decorations = true;
+	export let nested = false;
+
+	const element = href && !nested ? "a" : "span";
+</script>
+
+<svelte:element
+	this={element}
+	{href}
+	class="inline font-medium text-iris underline-offset-2 after:inline-block after:whitespace-pre hover:underline"
+	class:link={decorations}><slot /></svelte:element
+>
+
+<style lang="postcss">
+	.link {
+		@apply after:content-['_→'];
+	}
+	.link[href^="http"] {
+		@apply after:content-['_↗'];
+	}
+</style>
--- apps/service/src/peer/identity.rs
+++ apps/service/src/peer/identity.rs
@@ -1,7 +1,6 @@
 use anyhow::Result;
 use iroh::{Endpoint, SecretKey};
 
-#[tokio::main]
 async fn main() -> Result<()> {
     let secret_key: SecretKey = SecretKey::generate(rand::rngs::OsRng);
     println!("secret_key {secret_key}");

9c3146c8de60ba5e120995e347d968ba4ae2f4f0
veryCrunchy <me@verycrunchy.dev>
2025-07-04 06:20:41 +0200

    tmp-fix: disable spotlight

--- apps/client/astro.config.mjs
+++ apps/client/astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry(), spotlightjs()],
+  // integrations: [sentry(), spotlightjs()],
 
   vite: {
     plugins: [tailwindcss()]

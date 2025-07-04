f57d69f6b3601b2b32207549bf0ab5a4a9729bbb
veryCrunchy <me@verycrunchy.dev>
2025-07-04 09:02:23 +0200

    feat: Add @astrojs/node dependency and configure adapter in astro.config.mjs

--- apps/client/astro.config.mjs
+++ apps/client/astro.config.mjs
@@ -6,11 +6,18 @@ import spotlightjs from '@spotlightjs/astro';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   // integrations: [sentry(), spotlightjs()],
   output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });

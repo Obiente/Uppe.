e6bc44360b56508a1081cb51270fc6bf3f189a52
veryCrunchy <me@verycrunchy.dev>
2025-07-05 02:30:42 +0200

    config chores

--- apps/client/astro.config.mjs
+++ apps/client/astro.config.mjs
@@ -1,23 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import sentry from '@sentry/astro';
-import spotlightjs from '@spotlightjs/astro';
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   // integrations: [sentry(), spotlightjs()],
-  output: 'server',
+  output: "server",
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: "standalone",
+  }),
 });
